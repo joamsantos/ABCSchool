031a08b31e7ee0be4564dbb3c8843d48b4b51528
João Santos <joao.santos@vfxplc.com>
2026-02-18 23:20:32 +0000

    Frontend project started
    Frontend project started

---
+++ frontend/src/Infrastructure/ApiSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure;
+
+public class ApiSettings
+{
+    public string BaseApiUrl { get; set; }
+    public TokenEndpoints TokenEndpoints { get; set; }
+}
+
+public class TokenEndpoints
+{
+    public string GetToken { get; set; }
+    public string GetRefreshToken { get; set; }
+}
